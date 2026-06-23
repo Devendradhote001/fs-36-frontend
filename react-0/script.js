ae2dfe28e5ec77f0cc6b21e06ea9d91c64969744
Devendradhote001 <ddhote780@gmail.com>
2026-06-23 19:43:08 +0530

    asdfsa

--- react-0/script.js
+++ react-0/script.js
@@ -1,89 +1,129 @@
-// console.log("hello");
+// // console.log("hello");
 
-// let a = 40;
+// // let a = 40;
 
-// let sum = (num1, num2) => {
-//   let response = num1 + num2;
-//   return response;
+// // let sum = (num1, num2) => {
+// //   let response = num1 + num2;
+// //   return response;
+// // };
+
+// // let c = sum(46, 91);
+
+// // let fn = (...args) => {
+// //   console.log(args);
+// //   //   return a;
+// // };
+
+// // let res = fn(4, 6, 7, 8, 9);
+
+// // console.log(res);
+
+// // destructuring...
+
+// // let { age, address } = obj;
+
+// // console.log(age, address);
+
+// // let p = {
+// //   id: 1,
+// //   title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+// //   price: 109.95,
+// //   description:
+// //     "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+// //   category: "men's clothing",
+// //   image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
+// //   rating: {
+// //     rate: 3.9,
+// //     count: 120,
+// //   },
+// // };
+
+// // let {
+// //   rating: { rate },
+// //   id,
+// //   price,
+// // } = p;
+
+// // console.log(rate, id, price);
+
+// // let arr = [
+// //   "zeel",
+// //   "toi",
+// //   {
+// //     name: "rahul",
+// //   },
+// // ];
+
+// // let arr = [56, 90];
+
+// // let [point, score] = arr;
+
+// // console.log(score, point);
+
+// let fn = ({ name, age }) => {
+//   console.log(name, age);
 // };
 
-// let c = sum(46, 91);
-
-// let fn = (...args) => {
-//   console.log(args);
-//   //   return a;
-// };
-
-// let res = fn(4, 6, 7, 8, 9);
-
-// console.log(res);
-
-// destructuring...
-
-// let { age, address } = obj;
-
-// console.log(age, address);
-
-// let p = {
-//   id: 1,
-//   title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-//   price: 109.95,
-//   description:
-//     "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-//   category: "men's clothing",
-//   image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
-//   rating: {
-//     rate: 3.9,
-//     count: 120,
-//   },
-// };
-
-// let {
-//   rating: { rate },
-//   id,
-//   price,
-// } = p;
-
-// console.log(rate, id, price);
-
-// let arr = [
-//   "zeel",
-//   "toi",
-//   {
-//     name: "rahul",
-//   },
-// ];
-
-// let arr = [56, 90];
-
-// let [point, score] = arr;
-
-// console.log(score, point);
-
-let fn = ({ name, age }) => {
-  console.log(name, age);
-};
-
-fn({
-  name: "Raghav",
-  age: 89,
-  address: "Saket nagar",
-  company: "Google",
-});
-
-// let obj = {
+// fn({
 //   name: "Raghav",
 //   age: 89,
 //   address: "Saket nagar",
 //   company: "Google",
-// };
+// });
 
-// console.log(obj);
+// // let obj = {
+// //   name: "Raghav",
+// //   age: 89,
+// //   address: "Saket nagar",
+// //   company: "Google",
+// // };
 
-let arr1 = [2, 4, 5, 6];
+// // console.log(obj);
 
-let arr2 = ["hello", "polo", "taigun"];
+// let arr1 = [2, 4, 5, 6];
 
-let newArr = arr1;
+// let arr2 = ["hello", "polo", "taigun"];
 
-console.log(newArr);
+// let newArr = arr1;
+
+// console.log(newArr);
+
+// let a = 70;
+
+// console.log(a);
+
+// setTimeout(() => {
+//   console.log("chle jaa");
+// }, 0);
+
+// console.log("ajaaa");
+
+// promises------
+
+console.log("start");
+
+const prom = new Promise((res, rej) => {
+  let party = false;
+
+  if (party) {
+    return res({
+      data: {
+        nam: "TAj",
+      },
+    });
+  }
+  return rej("no, pese nahi hai...");
+});
+
+console.log("mid");
+
+prom.then((val) => {
+    console.log("then->", val);
+  })
+  .catch((err) => {
+    console.log("catch->", err);
+  });
+
+console.log(prom);
+
+console.log("end");
